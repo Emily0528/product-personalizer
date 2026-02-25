4fb18064081641de09bb3d81a8b1aefcd9816dae
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-25 15:31:30 +0100

    Add propTypes and use props for title and basePrice

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,7 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 
 const Product = props => {
   return (
@@ -13,8 +14,10 @@ const Product = props => {
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>
+            Price: {props.basePrice}$
+          </span>
         </header>
         <form>
           <div className={styles.sizes}>
@@ -41,6 +44,18 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  id: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  name: PropTypes.string,
+  title: PropTypes.string,
+  colors: PropTypes.array,
+  sizes: PropTypes.array,
+  basePrice: PropTypes.number,
 };
 
 export default Product;
